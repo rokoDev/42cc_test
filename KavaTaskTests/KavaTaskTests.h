e5c3e707f2c83e00437a56f52c855b0dedaaaaf0
rokoDev <mi-han@inbox.ru>
2013-06-13 19:16:52 +0300

    Initial Commit

---
+++ KavaTaskTests/KavaTaskTests.h
@@ -0,0 +1,13 @@
+//
+//  KavaTaskTests.h
+//  KavaTaskTests
+//
+//  Created by roko on 13.06.13.
+//  Copyright (c) 2013 roko. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KavaTaskTests : SenTestCase
+
+@end
---
+++ KavaTaskTests/KavaTaskTests.m
@@ -0,0 +1,32 @@
+//
+//  KavaTaskTests.m
+//  KavaTaskTests
+//
+//  Created by roko on 13.06.13.
+//  Copyright (c) 2013 roko. All rights reserved.
+//
+
+#import "KavaTaskTests.h"
+
+@implementation KavaTaskTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in KavaTaskTests");
+}
+
+@end
